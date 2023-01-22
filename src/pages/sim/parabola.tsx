be7170db2cd021fce9b0e85beb05846b42ad4d33
Shiv <sshivvss@gmail.com>
2023-01-22 18:53:08 +0530

    feat: more dynamic

--- src/pages/sim/parabola.tsx
+++ src/pages/sim/parabola.tsx
@@ -1,4 +1,4 @@
-import { Mafs, CartesianCoordinates, Plot } from "mafs"
+import { CartesianCoordinates, MovablePoint, Plot, Transform, UseMovablePoint, useMovablePoint } from "mafs"
 import { useState } from "react"
 import Layout from "../components/layout"
 
@@ -9,7 +9,15 @@ interface ICoEfficients{
   c: number
 }
 
-interface ISolvable extends ICoEfficients{
+interface ICoEfficientsWCTS{
+  a: number,
+  b: number,
+  c: number,
+  h: number,
+  k: number
+}
+
+interface ISolvable extends ICoEfficientsWCTS{
   x: number
 }
 
@@ -23,79 +31,121 @@ function resolve({a,b,c,x}: ISolvable){
 
 function sign(value: number): string{
   if(value > 0){
-  return `+${value}`
+  return `+ ${value}`
   }else {
     return String(value)
   }
   
 }
 
+function expand(a: number, h: number, k: number){
+  let b = 2*h;
+  let c = Math.pow(h,2) + k
+
+  return [b,c]
+}
+
 // Represent quadratic equation in completing the square form
-function cts({a,b,c}:ICoEfficients){
+function cts({a,b,c,h,k}:ICoEfficientsWCTS){
 
-  let d = b /(2*a);
-  let e = c - (Math.pow(b,2)/(4*a));
-
-  d = Math.round((d + Number.EPSILON) * 100) / 100
-  e = Math.round((e + Number.EPSILON) * 100) / 100
-  
-  return `${a != 1 ? a : ""}(x${ d != 0 ? sign(d): ""})² ${e != 0 ? sign(e): ""}`
+  return `${a != 1 ? a : ""}(𝑥 ${ h != 0 ? sign(h): ""})² ${k != 0 ? sign(k): ""}`
 
 }
 
-
-
-
-
-
-
-
+function calculateH({a,b}: ICoEfficients){
+   return (b/(2*a))
+}
+function calculateK({a,b,c}: ICoEfficients){
+  return c -((Math.pow(b,2))/(4*a))
+}
 // Graph
-function Graph({vars}: {vars: ICoEfficients}){
-  const {a,b,c} = vars;
+function Graph({vars, moving}: {vars: ICoEfficientsWCTS, moving: UseMovablePoint}){
+  const {a,b,c,h,k} = vars;
+ 
   return <>
    <CartesianCoordinates />
-   <Plot.OfX y={(x) => resolve({a,b,c,x})} />
+   <Plot.OfX y={(x) => resolve({a,b,c,h,k,x})} />
+   {}
    </>
 }
 
 export default function Parabola() {
   
   let [a,setA] = useState(1)
-  let [b,setB] = useState(1)
-  let [c,setC] = useState(1)
+  let [h,setH] = useState(calculateH({a,b: 1, c: 1}))
+  let [k,setK] = useState(calculateK({a,b: 1,c: 1}))
+
+  let b = expand(a,h,k)[0]
+  let c = expand(a,h,k)[1]
+  const turning = useMovablePoint([h,k], {
+    constrain: "vertical"
+  })
 
   return (
-    <Layout math={<Graph vars={{a,b,c}}/>}>
+    <Layout math={<Graph vars={{a,b,c,h,k}} moving={turning}/>}>
       <div className="flex  space-x-12 text-xl justify-between">
         <div>
-          <h1 className="font-math">{a}x² + {b}x + {c}</h1>
-          <h1 className="font-math">{cts({a,b,c})}</h1>
+          <h1 className="font-math">{a}𝑥² + {b}𝑥 + {c}</h1>
+          <h1 className="font-math">{cts({a,b,c,h,k})}</h1>
         </div>
       <div className="flex flex-col w-1/2 space-y-4">
       <input
           type="range"
-          min={-12}
+          min={1}
           step={1}
           max={12}
           value={a}
-          onChange={(event) => setA(+event.target.value)}
+          onChange={(event) => {
+            let newA = +event.target.value
+            setA(newA)
+
+          }}
         />
         <input
           type="range"
           min={-12}
           step={1}
           max={12}
-          value={b}
-          onChange={(event) => setB(+event.target.value)}
+          value={h}
+          onChange={(event) => 
+            {
+              let newH = +event.target.value
+              setH(newH)
+          }}
         /><input
         type="range"
         min={-12}
         step={1}
         max={12}
-        value={c}
-        onChange={(event) => setC(+event.target.value)}
+        value={k}
+        onChange={(event) => {
+          let newK = +event.target.value
+          setK(newK)
+        }}
       />
+      <input
+        type="range"
+        min={-12}
+        step={1}
+        max={12}
+        value={b}
+        onChange={(event) => {
+          let newB = +event.target.value
+          setH(calculateH({a,b: newB, c}))
+          setK(calculateK({a,b: newB, c}))
+        }}
+      /><input
+      type="range"
+      min={-12}
+      step={1}
+      max={12}
+      value={c}
+      onChange={(event) => {
+        let newC = +event.target.value
+        setH(calculateH({a,b, c: newC}))
+        setK(calculateK({a,b, c: newC}))
+      }}
+    />
       </div>
         
       </div>
